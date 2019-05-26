3a42454fa0f40a0f0de03a00d5f18f8f65afe99c
Alper Enes DURU <43013844+Alper-Enes-DURU@users.noreply.github.com>
2019-05-26 12:15:40 -0700

    Find Prime Number

---
+++ code.cpp
@@ -0,0 +1,38 @@
+#include <cctype>
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <stdio.h>
+#include <ctype.h>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+
+int main()
+{
+   	int n = 0;
+   	int i = 2;
+   	bool is_prime = true;
+
+   	cout << "Enter a number and press ENTER: ";
+   	cin >> n;
+
+   	while(i <= sqrt(n)){
+   		if(n % i == 0){
+   			is_prime = false;
+   			break;
+   		}
+   		i++;
+   	} 
+
+   	if(is_prime){
+   		cout << "Number is prime." << endl;
+   	}
+   	else{
+   		cout << "Number is not prime." << endl;
+   	}
+
+    return 0;
+}
+  
